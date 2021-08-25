fa014ee3ea67d307f20b73394b2a696c21a75d79
Nic Zagame <20585@my4county.net>
2021-08-25 09:53:57 -0400

    Messup

---
+++ src/ascii/ASCII.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ascii;
+
+/**
+ *
+ * @author nzagame
+ */
+public class ASCII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Top Half Of The Bow
+        System.out.println("          4$$-.");
+        System.out.println("           4   \".");
+        System.out.println("           4    ^.");
+        System.out.println("           4     $");
+        System.out.println("           4     'b");
+        System.out.println("           4      \"b.");
+        System.out.println("           4        $");
+        System.out.println("           4        $r");
+        System.out.println("           4        $F");
+        //Start Of the Arrow
+        System.out.println("-$b========4========$b====*P=->");
+        //Bottom Half Of the Bow
+        System.out.println("           4       *$$F");
+        System.out.println("           4        $$");
+        System.out.println("           4       .$F");
+        System.out.println("           4       dP");
+        System.out.println("           4      F");
+        System.out.println("           4     @");
+        System.out.println("           4   _.");
+        System.out.println("           J.-");
+        System.out.println("          '$$");
+        
+        
+    }
+    
+}
